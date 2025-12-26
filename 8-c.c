1c45dbf8cb687577acea480d42c67423538443f6
Shahriyar-Rahim <rahimislam420.ri35@gmail.com>
2025-12-27 02:29:43 +0600

    Co-authored-by: shammiayeman <shammiayeman@users.noreply.github.com>

--- 8-c.c
+++ 8-c.c
@@ -10,12 +10,38 @@ int main() {
     }
 
     char ch;
-    while ((ch = fgetc(input)) != EOF) {
+    while ((ch = fgetc(input)) != EOF) {  //hello\0
         fputc(ch, output);
         putchar(ch);
     }
 
     fclose(input);
     fclose(output);
+    return 0;
+}
+
+#include <stdio.h>
+
+int main() {
+    FILE *input = fopen("in.txt", "r");
+
+    char ch;
+    while ((ch = fgetc(input)) != EOF) {  //hello\0
+        putchar(ch);
+    }
+
+    fclose(input);
+    return 0;
+}
+
+int main(){
+    FILE *input = fopen("in.txt", "r");
+
+    char ch[80];
+    fgets(ch, 80, input);
+
+    printf("%s", ch);
+
+    fclose(input);
     return 0;
 }
